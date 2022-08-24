336abc2b9a84e75f2ee473aa1c741523b7471c2f
lucaspires-source <pires.lucas94@gmail.com>
2022-08-24 13:38:33 -0300

    adding phosphor icons

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,3 +1,20 @@
+import "./styles.scss";
+
+import { GoogleLogo } from "phosphor-react";
 export function SignIn() {
-  return <h1>Sign In </h1>;
+  return (
+    <div className="container">
+      <h1>Acesse sua conta</h1>
+      <span>
+        Utilizando autentificação social, por exemplo, autentificação com a
+        Google você <br />
+        facilita a vida do usuário, o permitindo utilizar sua aplicação sem
+        fazer cadastro.
+      </span>
+      <button type="button" className="button">
+        <GoogleLogo />
+        Entrar com Google
+      </button>
+    </div>
+  );
 }
